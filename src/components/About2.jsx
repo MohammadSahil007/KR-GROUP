c2f8c7312c4575ae28ba0273e60ad4a1de22028f
Someone-unknown-404 <123530249+Someone-unknown-404@users.noreply.github.com>
2024-07-09 17:23:47 +0530

    Initial commit

---
+++ src/components/About2.jsx
@@ -0,0 +1,111 @@
+import React from 'react';
+import './About.css';
+
+const AboutUs = () => {
+  return (
+    <div className="about-us">
+    <div style={{marginTop:100}} className="section-title text-center">
+      <h2 > About Us</h2>
+    </div>
+    <div>
+     <h2 style={{display:'block',textAlign:'left',paddingLeft:40,}}></h2>
+    </div>
+      <div className="section">
+      <img  src="img/about/kr2.png" alt="Our Founder" />
+        <div className="section-content">
+        <h2>Our Founder - <spans style={{color:'gold'}} >Khairullah Rafiullah Ansari   </spans> </h2>
+          <p style ={{fontSize:20}}>
+        Khairullah Rafiullah Ansari , the visionary founder of KR Group, established the company with a mission to bridge the talent gap across borders. With over two decades of experience in international recruitment and talent management, Khairullah Ansari has been a driving force in connecting businesses with top-tier professionals worldwide. His deep understanding of the global job market, coupled with a passion for helping individuals find their dream careers, has positioned KR Group as a leader in the recruitment industry.          </p>
+        </div>
+        </div>
+        
+      <div className="section">
+        <img src="img/about/fk.png" alt="Our CEO" />
+        <div className="section-content">
+          <h2> <spans style={{color:'gold'}} >Mohammad Farooque Khairullah Ansari   </spans> </h2>
+          <p  style ={{fontSize:20}}>
+          In the realm of air conditioning maintenance, we understand the critical role a well-functioning AC system plays in ensuring comfort and efficiency in both residential and commercial settings. Our facility management services are designed to streamline operations and enhance the overall functionality of your property. We offer a broad spectrum of services that cover everything from routine building maintenance, janitorial services, and landscaping to more specialized tasks such as security management and energy efficiency consulting.
+                    </p>  
+
+        </div>
+      </div>
+
+      <div className="section">
+      <img src="img/about/ak.png" alt="Our CFO" />
+        <div className="section-content">
+          <h2><spans style={{color:'gold'}} >Mohammad Ayub Khairullah Ansari   </spans> </h2>
+
+            <p style ={{fontSize:19}}>
+            Welcome to K.R. Group's Asian Recruitment services, guided by Mohammad Ayub Khairullah Ansari's expertise. We specialize in connecting businesses across Asia with exceptional talent through customized recruitment solutions. With a deep understanding of regional dynamics and industry nuances, we excel in identifying and placing skilled professionals in various sectors. Our commitment to quality and client satisfaction ensures that every placement is strategically aligned with organizational goals. Whether you're seeking top-tier executives or specialized technical experts, trust Mohammad Ayub Khairullah Ansari and K.R. Group to deliver reliable and effective recruitment outcomes tailored to your business needs.
+          </p>
+        </div>
+      </div>
+      <div className="section">
+      <img src="img/about/w2.png" alt="Our CFO" />
+        <div className="section-content">
+          <h2><spans style={{color:'gold'}} >Mohammad Wasim Khairullah Ansari</spans> </h2>
+
+            <p style ={{fontSize:20}}>
+          Welcome to K.R. Group's Ticketing and Travels division. I am Mohammad Wasim Khairullah Ansari, and I manage our travel services with a commitment to excellence. We specialize in providing seamless travel solutions, including flight bookings, hotel reservations, and comprehensive travel packages tailored to meet your needs. With a focus on exceptional customer service and competitive pricing, my team ensures a hassle-free travel experience from start to finish. Whether for business or leisure, trust K.R. Group's Ticketing and Travels to handle all your travel arrangements with professionalism and care.
+          </p>
+        </div>
+      </div>
+      <div className="section">
+      <img src="img/about/nasim.png" alt="Our CFO" />
+        <div className="section-content">
+          <h2>  <spans style={{color:'gold'}} >Mohammad Nasim Khairullah Ansari</spans> </h2>
+
+            <p style ={{fontSize:20}}>
+            I am Mohammad Nasim Khairullah Ansari, leading Overseas Recruitment at K.R. Group. With a dedicated focus on connecting businesses with top-tier global talent, my team and I specialize in providing tailored recruitment solutions across diverse industries. We leverage extensive experience and a strong international network to source, vet, and place skilled professionals worldwide. At K.R. Group, our commitment to precision, integrity, and client satisfaction ensures seamless placements that drive long-term success for both employers and candidates alike. Partner with us for your next strategic recruitment initiative and experience the difference with Mohammad Nasim Khairullah Ansari and K.R. Group.
+          </p>
+        </div>
+      </div>
+      <div className="section">
+      <img src="img/about/skr.png" alt="Our CFO" />
+        <div className="section-content">
+          <h2><spans style={{color:'gold'}} >Mohammad Shadab Khairullahh Ansari   </spans> </h2>
+
+            <p style ={{fontSize:20}}>
+            Welcome to K.R. Group's Hajj and Umrah services, managed by Mohammad Shadab Khairullah Ansari. We specialize in offering comprehensive pilgrimage packages that ensure a seamless and spiritually enriching journey. Our services include visa assistance, flight bookings, comfortable accommodations, and guided tours to the holy sites. With a focus on customer satisfaction and attention to detail, we strive to make your pilgrimage experience smooth, safe, and memorable. Trust K.R. Group for a hassle-free and deeply rewarding Hajj and Umrah journey.
+          </p>
+        </div>
+      </div>
+      <div className="section2">
+      {// <img width={700} height={300} style={{border:'2px solid gold', }} src="img/about/Story.jpg" alt="Our Story" />
+      }
+      <div  className="section-content-2">
+          <h2 style={{display:'block',paddingTop:50,textAlign:'center'}}>Our Story </h2>
+
+          <p style={{display:'block',fontSize:19,textAlign:'center'}}>
+          Nestled in the heart of our town, KR Group is more than just an international recruitment company; it's a celebration of our community's vibrant spirit. We take pride in connecting talent across borders, reflecting the rich tapestry of our global culture. Every placement we make is a labor of love, a blend of tradition and innovation, and a commitment to the people who trust us with their careers. At KR Group, we're more than recruiters; we're storytellers, weaving the essence of our town into every connection.
+          </p>
+        </div>
+      </div>
+      <div className="section2">
+      {// <img width={700} height={300} style={{border:'2px solid gold', }} src="img/about/Story.jpg" alt="Our Story" />
+      }
+      <div  className="section-content-2">
+          <h2 style={{display:'block',paddingTop:50,textAlign:'center'}}>Our Mission </h2>
+
+          <p style={{display:'block',fontSize:19,textAlign:'center'}}>
+          At KR Group, our mission is clear to bridge the gap between exceptional talent and outstanding opportunities worldwide. We're committed to using our extensive network and deep industry knowledge to create a platform where the best talent meets the best opportunities. Beyond our passion for recruitment, we aim to be a welcoming hub for professionals, creating a space for growth, connection, and success. Our mission is to share the joy of career building, one successful placement at a time, as we celebrate the incredible individuals who make this possible.
+          </p>
+        </div>
+      </div>
+      <div className="section2">
+      {// <img width={700} height={300} style={{border:'2px solid gold', }} src="img/about/Story.jpg" alt="Our Story" />
+      }
+      <div  className="section-content-2">
+          <h2 style={{display:'block',paddingTop:50,textAlign:'center'}}>Our Philosophy </h2>
+
+          <p style={{display:'block',fontSize:19,textAlign:'center',paddingBottom:50}}>
+          At KR Group, our philosophy is rooted in trust, integrity, and excellence. We believe in the power of global talent and the positive impact it can have on businesses and communities. Our commitment is to connect people with opportunities that allow them to thrive, using the finest practices and time-honored techniques to ensure the best matches. We strive to build a community around our services, where every placement is a testament to our dedication and passion for helping individuals and organizations succeed.
+          </p>
+        </div>
+      </div>
+
+    </div>
+  );
+};
+
+export default AboutUs;
---
+++ src/components/ourClients.jsx
@@ -0,0 +1,246 @@
+// import React from "react";
+// import o1 from './ourClients/o1.png'
+// import o2 from './ourClients/o2.png'
+// import o3 from './ourClients/o3.png'
+// export const IndustriesWeServe = (props) => {
+//   return (
+//     <div id="services"  >
+//       <div className="container">
+//         <div className="section-title text-center">
+//           <h2>Our Clients</h2>
+//         </div>
+//         <div  style={{display:'block'}} className="col-md-4">
+//                   <img style={{borderRadius:10,marginBottom:'50'}} src={o1} width={250} height={100}  />
+//                   <div className="service-desc">
+//                     <span style ={{ fontFamily:'"Raleway",sans-serif',display:'block',fontSize:25,fontWeight:'bold', alignContent:'center', textAlign:'center'}}>Saudi</span>
+//                   </div>
+//         </div>
+//         <div className="col-md-4">
+//                   <img style={{borderRadius:10,marginBottom:'50'}} src={o2} width={250} height={100} />
+//                   <div className="service-desc">
+//                     <span style ={{ fontFamily:'"Raleway",sans-serif',display:'block',fontSize:25,fontWeight:'bold', alignContent:'center', textAlign:'center'}}>Saudi</span>
+//                   </div>
+//         </div>
+//         <div  className="col-md-4">
+//                   <img style={{borderRadius:10}} src={o3} width={150} height={150} />
+//                   <div className="service-desc">
+//                     <span style ={{ fontFamily:'"Raleway",sans-serif',display:'block',fontSize:25,fontWeight:'bold', alignContent:'center', textAlign:'center'}}>Saudi</span>
+//                   </div>
+//         </div>
+//         <div  className="col-md-4">
+//                   <img style={{borderRadius:10}} src={o1} width={250} height={100} />
+//                   <div className="service-desc">
+//                     <span style ={{ fontFamily:'"Raleway",sans-serif',display:'block',fontSize:25,fontWeight:'bold', alignContent:'center', textAlign:'center'}}>Saudi</span>
+//                   </div>
+//         </div>
+//         <div  className="col-md-4">
+//                   <img style={{borderRadius:10}} src={o1} width={250} height={100} />
+//                   <div className="service-desc">
+//                     <span style ={{ fontFamily:'"Raleway",sans-serif',display:'block',fontSize:25,fontWeight:'bold', alignContent:'center', textAlign:'center'}}>Saudi</span>
+//                   </div>
+//         </div>
+//         <div  className="col-md-4">
+//                   <img style={{borderRadius:10}} src={o1} width={250} height={100} />
+//                   <div className="service-desc">
+//                     <span style ={{ fontFamily:'"Raleway",sans-serif',display:'block',fontSize:25,fontWeight:'bold', alignContent:'center', textAlign:'center'}}>Saudi</span>
+//                   </div>
+//         </div>
+//         <div  className="col-md-4">
+//                   <img style={{borderRadius:10}} src={o1} width={250} height={100} />
+//                   <div className="service-desc">
+//                     <span style ={{ fontFamily:'"Raleway",sans-serif',display:'block',fontSize:25,fontWeight:'bold', alignContent:'center', textAlign:'center'}}>Saudi</span>
+//                   </div>
+//         </div>
+//         <div  className="col-md-4">
+//                   <img style={{borderRadius:10}} src={o1} width={250} height={100} />
+//                   <div className="service-desc">
+//                     <span style ={{ fontFamily:'"Raleway",sans-serif',display:'block',fontSize:25,fontWeight:'bold', alignContent:'center', textAlign:'center'}}>Saudi</span>
+//                   </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+// export default IndustriesWeServe;
+// import React from "react";
+// import o1 from './ourClients/o1.png';
+// import o2 from './ourClients/o2.png';
+// import o3 from './ourClients/o3.png';
+// import o4 from './ourClients/o4.png';
+// import o5 from './ourClients/o5.png';
+// import o6 from './ourClients/o6.png';
+// import o7 from './ourClients/o7.png';
+// import o8 from './ourClients/o8.png';
+// import o9 from './ourClients/o9.png';
+// import o10 from './ourClients/o10.png';
+// import o11 from './ourClients/o11.png';
+// import o12 from './ourClients/o12.png';
+// import o13 from './ourClients/o13.png';
+// import o14 from './ourClients/o14.png';
+// // import o3 from './ourClients/o3.png';
+
+// export const IndustriesWeServe = (props) => {
+//   return (
+//     <div id="services">
+//       <div className="container">
+//         <div className="section-title text-center">
+//           <h2>Our Clients</h2>
+//         </div>
+//         <div className="logo-panel">
+//           <div className="logo-item">
+//             <img style={{ borderRadius: 10, marginBottom: 50 }} src={o1} width={250} height={100} />
+//             <div className="service-desc">
+//               <span className="logo-text">Saudi</span>
+//             </div>
+//           </div>
+//           <div className="logo-item">
+//             <img style={{ borderRadius: 10, marginBottom: 30 }} src={o13} width={150} height={120} />
+//             <div className="service-desc">
+//               <span className="logo-text">Saudi</span>
+//             </div>
+//           </div>
+//           <div className="logo-item">
+//             <img style={{ borderRadius: 10 }} src={o3} width={150} height={150} />
+//             <div className="service-desc">
+//               <span className="logo-text">Saudi</span>
+//             </div>
+//           </div>
+//         </div>
+//         <div className="logo-panel">
+//           <div className="logo-item">
+//             <img style={{ borderRadius: 10, marginBottom: 30 }} src={o4} width={250} height={120} />
+//             <div className="service-desc">
+//               <span className="logo-text">Saudi</span>
+//             </div>
+//           </div>
+//           <div className="logo-item">
+//             <img style={{ borderRadius: 10, marginBottom: 0 }} src={o5} width={160} height={150} />
+//             <div className="service-desc">
+//               <span className="logo-text">Saudi</span>
+//             </div>
+//           </div>
+//           <div className="logo-item">
+//             <img style={{ borderRadius: 10,marginBottom:30 }} src={o14} width={150} height={120} />
+//             <div className="service-desc">
+//               <span className="logo-text">Saudi</span>
+//             </div>
+//           </div>
+//         </div>
+//         <div className="logo-panel">
+//           <div className="logo-item">
+//             <img style={{ borderRadius: 10,  }} src={o7} width={200} height={150} />
+//             <div className="service-desc">
+//               <span className="logo-text">Saudi</span>
+//             </div>
+//           </div>
+//           <div className="logo-item">
+//             <img style={{ borderRadius: 10 }} src={o8} width={150} height={150} />
+//             <div className="service-desc">
+//               <span className="logo-text">Saudi</span>
+//             </div>
+//           </div>
+//           <div className="logo-item">
+//             <img style={{ borderRadius: 10 }} src={o9} width={150} height={150} />
+//             <div className="service-desc">
+//               <span className="logo-text">Saudi</span>
+//             </div>
+//           </div>
+//         </div>
+//         <div className="logo-panel">
+//           <div className="logo-item">
+//             <img style={{ borderRadius: 10, marginBottom: 50 }} src={o10} width={200} height={120} />
+//             <div className="service-desc">
+//               <span className="logo-text">Saudi</span>
+//             </div>
+//           </div>
+//           <div className="logo-item">
+//             <img style={{ borderRadius: 10, marginBottom: 30 }} src={o12} width={180} height={120} />
+//             <div className="service-desc">
+//               <span className="logo-text">Saudi</span>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default IndustriesWeServe;
+
+
+
+import React from "react";
+import o1 from './ourClients/o1.png';
+import o2 from './ourClients/o2.png';
+import o3 from './ourClients/o3.png';
+import o4 from './ourClients/o4.png';
+import o5 from './ourClients/o5.png';
+import o6 from './ourClients/o6.png';
+import o7 from './ourClients/o7.png';
+import o8 from './ourClients/o8.png';
+import o9 from './ourClients/o9.png';
+import o10 from './ourClients/o10.png';
+import o11 from './ourClients/o11.png';
+import o12 from './ourClients/o12.png';
+import o13 from './ourClients/o13.png';
+import o14 from './ourClients/o14.png';
+
+export const IndustriesWeServe = (props) => {
+  return (
+    <div id="services">
+      <div className="container">
+        <div className="section-title text-center">
+          <h2>Our Clients</h2>
+        </div>
+        <div className="logo-grid">
+          <div className="logo-item">
+            <img style={{ borderRadius: 10 }} src={o1} width={250} height={100} />
+             
+          </div>
+          <div className="logo-item">
+            <img style={{ borderRadius: 10 }} src={o13} width={150} height={120} />
+             
+          </div>
+          <div className="logo-item">
+            <img style={{ borderRadius: 10 }} src={o3} width={150} height={150} />
+             
+          </div>
+          <div className="logo-item">
+            <img style={{ borderRadius: 10 }} src={o4} width={250} height={120} />
+             
+          </div>
+          <div className="logo-item">
+            <img style={{ borderRadius: 10 }} src={o5} width={160} height={150} />
+             
+          </div>
+          <div className="logo-item">
+            <img style={{ borderRadius: 10 }} src={o14} width={150} height={120} />
+             
+          </div>
+          <div className="logo-item">
+            <img style={{ borderRadius: 10 }} src={o7} width={200} height={150} />
+             
+          </div>
+          <div className="logo-item">
+            <img style={{ borderRadius: 10 }} src={o8} width={150} height={150} />
+             
+          </div>
+          <div className="logo-item">
+            <img style={{ borderRadius: 10 }} src={o9} width={150} height={150} />
+             
+          </div>
+          <div className="logo-item">
+            <img style={{ borderRadius: 10 }} src={o10} width={200} height={120} />
+             
+          </div>
+          <div className="logo-item">
+            <img style={{ borderRadius: 10 }} src={o12} width={200} height={100} />
+             
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default IndustriesWeServe;
